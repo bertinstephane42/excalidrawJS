bceb7db1d578048918d91d181f320130ebd7970d
bertinstephane42 <73121940+bertinstephane42@users.noreply.github.com>
2025-09-08 14:02:59 +0200

    Add files via upload

--- inc/users.php
+++ inc/users.php
@@ -1,9 +1,20 @@
 <?php
-// Liste des utilisateurs (login => hash)
-$users = [
-	'admin' => '$2y$10$dYnbLLWp77rRaiOm/JszGO2ARh2p9oWsb/R4EnoqRdQ9vpX7TSzJ6', // Remplacer (admin par défaut)
-	'etudiant' => '$2y$10$40yyj6f5YrFj.j0WWo122Oh2x.7Dcf0Xjx4fc/qH4vIIMR9iATo0W', // Remplacer (etudiant par défaut)
-];
-	
-//echo password_hash('Password', PASSWORD_DEFAULT);
-?>
+/**
+ * Liste des utilisateurs.
+ * Chaque entrée : 'login' => ['password' => <hash>, 'role' => 'admin'|'user', 'disabled' => bool]
+ * NB : Le compte 'admin' est l’admin principal et NE PEUT PAS être supprimé ni désactivé.
+ */
+$users = array (
+  'admin' => 
+  array (
+    'password' => '$2y$10$YLGbpCzI4EEpbGhct9/Ts.WnUKLICfYxYW5I7oAWCR.iwcTacvUR2',
+    'role' => 'admin',
+    'disabled' => false,
+  ),
+  'etudiant' => 
+  array (
+    'password' => '$2y$10$IBSgPZeyFENf8x8LToYbuOCKrAwel1L6qwYQalqCb83X22ZuySnke',
+    'role' => 'user',
+    'disabled' => false,
+  ),
+);
